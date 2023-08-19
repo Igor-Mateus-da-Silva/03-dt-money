be9856e70c00dc287dc02a5ff8518398aa57b814
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2023-08-18 21:35:51 -0300

    add Transactions table
    ++Transactions table

--- pages/Transactions/index.tsx
+++ pages/Transactions/index.tsx
@@ -1,11 +1,69 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento do site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 1.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>18/08/2023</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Pizza</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - R$ 50,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>17/08/2023</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Desenvolvimento do site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 1.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>18/08/2023</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Desenvolvimento do site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 1.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>18/08/2023</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Desenvolvimento do site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 1.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>18/08/2023</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
